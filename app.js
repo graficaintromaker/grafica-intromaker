659226ac770cdac50e22dd0624064091c4f669eb
graficaintromaker <graficaintromaker@gmail.com>
2026-08-11 00:44:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ const products = [
     category:"promocionais",
     categoryLabel:"Promocionais",
     desc:"Crie recorrência com cartões de fidelidade personalizados para sua loja, salão ou serviço.",
-    image:"assets/07-cartao-fidelidade.jpg"
+    image:"assets/08-raspadinha.jpg"
   },
   {
     id:"raspadinhas",
