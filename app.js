47e68849a03dce726a7738828ea61207f65fef2d
graficaintromaker <graficaintromaker@gmail.com>
2026-08-14 20:16:15 -0300

    Atualiza versão do catálogo

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (() => {
   const base=document.createElement("script");
-    base.src="app-base.js?v=20260814-11";
+    base.src="app-base.js?v=20260814-12";
   base.onload=()=>{
     const extras=document.createElement("script");
-      extras.src="novos-cartoes.js?v=20260814-11";
+      extras.src="novos-cartoes.js?v=20260814-12";
     document.body.appendChild(extras);
   };
   document.body.appendChild(base);
