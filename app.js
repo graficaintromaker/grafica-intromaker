983f915777a2bcb7da04f43ac07934c122e2d9f6
graficaintromaker <graficaintromaker@gmail.com>
2026-08-14 18:50:12 -0300

    Atualiza versão dos arquivos do site

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (() => {
   const base=document.createElement("script");
-  base.src="app-base.js?v=20260814-7";
+  base.src="app-base.js?v=20260814-8";
   base.onload=()=>{
     const extras=document.createElement("script");
-    extras.src="novos-cartoes.js?v=20260814-7";
+    extras.src="novos-cartoes.js?v=20260814-8";
     document.body.appendChild(extras);
   };
   document.body.appendChild(base);
